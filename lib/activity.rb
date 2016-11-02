6a2fc983055c2182c2697a1e946948040ec61060
alanyeh20001 <alanyeh20001@hotmail.com>
2016-11-02 23:49:07 +0800

    complete activity fetch & save

---
+++ lib/activity.rb
@@ -0,0 +1,49 @@
+require "open-uri"
+require "json"
+require "yaml"
+
+class Activity
+  attr_accessor :activity_sort, :store_path
+  
+  def initialize(activity_path, store_path)
+    @store_path = store_path
+    activities_info = JSON.parse(open(activity_path).read)
+    sort(activities_info)
+  end
+  
+  def sort(activities)
+    @activity_sort = Hash.new{ |hash, key| hash[key] = [] }
+    
+    activities.each do |activity|
+      case activity["classname"]
+      when "藝文休閒"
+        @activity_sort["leisure"] << activity
+      when "青年體育"
+        @activity_sort["sport"] << activity
+      when "衛生保健"
+        @activity_sort["hygiene"] << activity
+      when "環境保護"
+        @activity_sort["environment"] << activity
+      when "社會福利"
+        @activity_sort["welfare"] << activity
+      when "教育"
+        @activity_sort["education"] << activity
+      when "交通觀光"
+        @activity_sort["tourism"] << activity
+      else "其他"
+        @activity_sort["others"] << activity
+      end
+    end
+    
+    self.save
+  end
+  
+  def save
+    system("touch", store_path) unless File.exist?(store_path)
+    
+    File.open(store_path, 'w') do |file|
+      file.write activity_sort.to_yaml
+    end
+  end
+end
+
---
+++ script/update.rb
@@ -0,0 +1,23 @@
+$: << "lib"
+
+require "optparse"
+
+require "activity"
+
+# Get output file of activities from command line.
+options = {}
+
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: script/update.rb [options] [FileName]"
+  
+  opts.on('-o File', '--output File', 'The output file of activities') do |file|
+    options[:output] = file
+  end
+end.parse!
+
+# Get activities from API & store them in output file. 
+activity_store_path = options[:output]
+
+activity_path = "http://data.ntpc.gov.tw/od/data/api/029E3FC2-1927-4534-8702-DA7323BE969B?$format=json"
+activities = Activity.new(activity_path, activity_store_path)
+
